8c845cfb0cbfd3e184ab7f8947c8480b003296fc
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-06 16:55:33 +0600

    implemented profile-update functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,11 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("update/", ProfileView.as_view(), name="update"),
+
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
