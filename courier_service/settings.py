dbad5526f91db4e9ad5d78eb682ed95018c18836
Solaiman Hossain <slmnsnto@gmail.com>
2025-07-08 15:25:43 +0600

    fix domain name

--- courier_service/settings.py
+++ courier_service/settings.py
@@ -149,8 +149,8 @@ from decouple import config
 STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY', default='')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY', default='')
 
-#DOMAIN = "https://courier-management-system-backend-api-53k6.onrender.com"
-DOMAIN = "http://127.0.0.1:8000"
+DOMAIN = "https://courier-management-system-backend-api-53k6.onrender.com"
+#DOMAIN = "http://127.0.0.1:8000"
 
 CSRF_TRUSTED_ORIGINS = [
     'https://courier-management-system-backend-api-53k6.onrender.com',
